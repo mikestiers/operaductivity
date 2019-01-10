fe1519a6cdb53457f973656fccd7535cdefc57c0
Mike Stiers <mike.stiers@gmail.com>
2019-01-09 19:07:45 -0500

    semi-transparent block with message

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var canvas = document.createElement('canvas');
+/* var canvas = document.createElement('canvas');
 canvas.style.width='100%';
 canvas.style.height='100%';
 canvas.width = window.innerWidth;
@@ -15,3 +15,11 @@ var context = canvas.getContext('2d');
 context.rect(50, 50, canvas.width-100, canvas.height-100);
 context.fillStyle = 'yellow';
 context.fill();
+*/ 
+var overlay = jQuery('<div id="overlay"> <div id="text"><h2>GET BACK TO WORK!</h2></div> </div>');
+//var overlayText =jQuery('<div id="text"><h2>Overlay with Text</h2></div>');
+overlay.appendTo(document.body)
+//overlayText.appendTo(document.body)
+
+
+
