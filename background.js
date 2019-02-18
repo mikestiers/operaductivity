219351860127d4252ada284488a58cb95b24fcfa
Mike Stiers <mike.stiers@gmail.com>
2019-02-18 08:45:00 -0500

    change dialogue and new tab

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function() {
-	alert("test")
+	alert("your goal should be to not need this button")
 	chrome.tabs.query({
 		currentWindow: true,
 		active: true
 	}, function(tab) {
 		chrome.tabs.create({
-			"url": "http://dev.opera.com"
+			"url": "https://en.wikipedia.org/wiki/Self-control"
 
 		});
 	});
